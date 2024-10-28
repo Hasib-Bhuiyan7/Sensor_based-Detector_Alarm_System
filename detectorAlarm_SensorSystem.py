b7f1324aca6679d196d1125452d4c73c7807bfba
Hasib Bhuiyan <hasib.bhuiyan@torontomu.ca>
2024-10-28 00:27:05 -0400

    Update detectorAlarm_SensorSystem.py

--- detectorAlarm_SensorSystem.py
+++ detectorAlarm_SensorSystem.py
@@ -1,5 +1,3 @@
-#This program replicates an intruder alarm/detector system
-#Using the raspberry Pi Pico as the microcontroller implemented onto the breadboard to interface with the other electrical devices such as the PIR motion sensor which gives inputs which are read and processed, subsequently triggering the alarm system
 #The alarm system consists of the three LEDs(Yellow, Blue, and Red) that light up in alternation dpeending on motion senser input and a piezzo buzzer produces a buzzing sound of certain frequency(500) when motion sensor is triggered 
 #The electrical circuit system consists of different electrical components; 3 LEDs, 6 220ohms resistors, motion sensor(GND, VCC, Sensor Input/data Pin) and a piezzo buzzer(+ and - terminals)
 #Slow alternating flashing indicates alarm is active but not triggered, otherwise quick alternating flashes between LEDs, buzzer sound initiated, and message output on the Shell indicates that alarm has been triggered by motion detected by the PIR sensor
